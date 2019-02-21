2ec097e31254d6afc7fb305905672b61db5e15a1
NerdyPepper <akshayoppiliappan@gmail.com>
2019-02-21 18:44:03 +0530

    switch to root em

--- components/footer.js
+++ components/footer.js
@@ -1,19 +1,22 @@
 import Link from 'next/link'
 
+// styles
+import '../styles/Footer.css'
+
 const Footer = () => (
     <div className="footer clearfix">
         <div className="box">
             <h4>Contact</h4>
             <p>+91 83100 32933</p>
-            <p><a href="mailto:jatayurvce@gmail.com">jatayurvce@gmail.com</a></p>
+            <p><a className="footerLink" href="mailto:jatayurvce@gmail.com">jatayurvce@gmail.com</a></p>
             <p>R V College of Engineering,</p>
             <p>Bengaluru, Karnataka</p>
         </div>
         <div className="box">
             <h4>Social</h4>
-            <p><a href="https://www.instagram.com/projectjatayu/">Instagram</a></p>
-            <p><a href="https://www.facebook.com/ProjectJatayu">Facebook</a></p>
-            <p><a href="https://www.linkedin.com/in/project-jatayu-rvce-a9b14781/?originalSubdomain=in">Linkedin</a></p>
+            <p><a className="footerLink" href="https://www.instagram.com/projectjatayu/">instagram.com/<b>projectjatayu</b></a></p>
+            <p><a className="footerLink" href="https://www.facebook.com/ProjectJatayu">facebook.com/<b>projectjatayu</b></a></p>
+            <p><a className="footerLink" href="https://www.linkedin.com/in/project-jatayu-rvce-a9b14781/?originalSubdomain=in">Linkedin</a></p>
         </div>
         <div className="box">
             <h4>More</h4>
@@ -27,21 +30,10 @@ const Footer = () => (
         </div>
         <style jsx>{`
             @import url('https://rsms.me/inter/inter.css');
-            html { font-family: 'Inter', sans-serif; padding: 0; margin: 0 }
+            html { font-family: 'Inter', sans-serif; padding: 0; margin: 0, font-size: 16px}
             body {
                 margin: 0;
                 padding: 0;
-            }
-            .footer {
-                position: relative;
-                background-color: #fff;
-                bottom: 0;
-                right: 0;
-                left: 0;
-                box-shadow: 0px -1em 4em -0.2em #efefef;
-                padding: 2em 10em 2em 10em;
-                color: #222;
-                font-family: "Inter";
             }
             .clearfix::after {
                 content: "";
@@ -53,21 +45,13 @@ const Footer = () => (
                 width: 33.33%;
             }
             h4 {
-                font-size: 18px;
+                font-size: 1rem;
                 font-weight: bold;
-            }
-            a {
-                color: #999;
-                text-decoration: none;
-                transition: 0.5s ease;
-            }
-            a:hover {
-                color: #222;
             }
             .bottom-logo {
                 font-weight: light;
-                font-size: 22px;
-                padding: 10em 0em 0em 0em;
+                font-size: 1.5rem;
+                padding: 15rem 0rem 0rem 0rem;
             }
                     `}
         </style>
